7987cce98bbc9cecb6a85704eff7dd1677162965
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-09-19 16:24:36 +0530

    made login and register page

--- client/src/Interface/interface.ts
+++ client/src/Interface/interface.ts
@@ -1,6 +1,18 @@
 export interface Post {
-    title?: string;
-    desc?: string;
-    language?:string;
-    code?:string;
-  }
+  title?: string;
+  desc?: string;
+  language?: string;
+  code?: string;
+}
+
+export interface login {
+  email?: string;
+  password?: string;
+}
+
+export interface SignUp {
+  fname?: string;
+  lname?: string;
+  email?: string;
+  password?: string;
+}
