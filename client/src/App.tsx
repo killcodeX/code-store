86e1801466003b69db0006e8002039ce61546a85
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-09-19 13:05:05 +0530

    made route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import "./App.css";
+import { Switch, Route, useHistory } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "./Theme/theme";
 import { useSelector } from "react-redux";
 import Home from "./Container/Home";
+import Login from "./Container/Auth/login";
+import Register from "./Container/Auth/register";
 
 function App() {
   const theme = useSelector((state: any) => state.post.theme);
   return (
     <ThemeProvider theme={theme === false ? darkTheme : lightTheme}>
       <div className="position-relative">
-        <Home />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/register" exact component={Register} />
+        </Switch>
       </div>
     </ThemeProvider>
   );
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 import Store from "./Redux/store";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={Store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
