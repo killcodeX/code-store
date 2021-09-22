9a12e7f3377dd0670d245355595be707c8dcf292
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-09-22 13:58:10 +0530

    pop confirmed appear before deleting

--- client/src/Components/CodeCards/index.tsx
+++ client/src/Components/CodeCards/index.tsx
@@ -14,28 +14,49 @@ import { Post } from "../../Interface/interface";
 import {
   getDisplayPostModal,
   getEditPostodal,
-  deletePost
+  deletePost,
 } from "./../../Redux/Actions/postActions";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { Popconfirm } from "antd";
 
 export default function CodeCard({ item }) {
   const dispatch = useDispatch();
 
+  function confirm(e) {
+    dispatch(deletePost(item._id));
+  }
+
+  function cancel(e) {
+    console.log("Post not removed");
+  }
+
   return (
     <CardWrapper>
       <CardTitle>{item.title}</CardTitle>
-      <CardDescription>{item.description.slice(0,120)+ "..."}</CardDescription>
+      <CardDescription>
+        {item.description.slice(0, 120) + "..."}
+      </CardDescription>
       <CardCategory>{item.language}</CardCategory>
       <OptionWrapper>
-        <DisplayIconWrapper onClick={() => dispatch(getDisplayPostModal(item._id))}>
+        <DisplayIconWrapper
+          onClick={() => dispatch(getDisplayPostModal(item._id))}
+        >
           <CgShutterstock />
         </DisplayIconWrapper>
         <EditIconWrapper onClick={() => dispatch(getEditPostodal(item._id))}>
           <CgPen />
         </EditIconWrapper>
-        <DeleteIconWrapper onClick={() => dispatch(deletePost(item._id))}>
-          <CgTrashEmpty />
-        </DeleteIconWrapper>
+        <Popconfirm
+          title="Are you sure to delete this task?"
+          onConfirm={confirm}
+          onCancel={cancel}
+          okText="Yes"
+          cancelText="No"
+        >
+          <DeleteIconWrapper>
+            <CgTrashEmpty />
+          </DeleteIconWrapper>
+        </Popconfirm>
       </OptionWrapper>
     </CardWrapper>
   );
